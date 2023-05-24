5d99246ed198dbd4ec070acb2b449e778f872d8b
kmou424 <me@kmou424.moe>
2023-05-24 17:48:36 +0800

    :test_tube: test(router): add version router test

---
+++ backend/server/routers/test/router_test.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"fmt"
+	"github.com/TakasakiApps/Narravo/backend/internal/global"
+	"github.com/TakasakiApps/Narravo/backend/server"
+	"github.com/TakasakiApps/Narravo/backend/server/engine"
+	"github.com/gookit/goutil/testutil/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestVersionRouter(t *testing.T) {
+	server.Registries()
+
+	req, err := http.NewRequest("GET", "/api/v1/version", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res := httptest.NewRecorder()
+	engine.Gin.ServeHTTP(res, req)
+
+	assert.Eq(t, http.StatusOK, res.Code)
+
+	assert.Eq(t,
+		fmt.Sprintf(`{"version":"%s"}`, global.GetVersion()),
+		res.Body.String(),
+	)
+}
