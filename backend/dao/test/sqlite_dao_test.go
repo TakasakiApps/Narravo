0242f51ca1d38ceedfd939687106853546cd5c61
kmou424 <me@kmou424.moe>
2023-05-27 17:44:53 +0800

    :sparkles:feat(database): add basic SQLite implementation and common DAO interface

---
+++ backend/dao/test/sqlite_dao_test.go
@@ -0,0 +1,77 @@
+package test
+
+import (
+	"encoding/json"
+	"github.com/TakasakiApps/Narravo/backend/dao"
+	"github.com/TakasakiApps/Narravo/backend/dao/sqlite"
+	"github.com/TakasakiApps/Narravo/backend/internal/database"
+	"github.com/TakasakiApps/Narravo/backend/internal/types"
+	"github.com/gookit/goutil/testutil/assert"
+	"github.com/ohanakogo/exceptiongo"
+	"testing"
+)
+
+var driver database.Driver
+
+type Test struct {
+	Id     int     `gorm:"primaryKey;autoIncrement"`
+	Param1 string  `gorm:"not null"`
+	Param2 int     `gorm:"not null"`
+	Param3 float64 `gorm:"not null"`
+}
+
+func TestSQLite(t *testing.T) {
+	driver = dao.NewDAO[sqlite.SQLite]("test.db")
+
+	// Test migrate
+	migrate()
+
+	// add
+	add()
+
+	// remove
+	remove()
+
+	// update
+	update()
+
+	// query
+	queryResult := query()
+
+	marshal, err := json.Marshal(queryResult)
+	exceptiongo.QuickThrow[types.JsonUnmarshalFailedException](err)
+	assert.Eq(t, `{"Id":2,"Param1":"updated","Param2":1,"Param3":3.14}`, string(marshal))
+}
+
+func migrate() {
+	driver.Migrate(&Test{})
+}
+
+func add() {
+	for i := 0; i < 3; i++ {
+		driver.GetDB().Model(&Test{}).Create(&Test{
+			Param1: "test",
+			Param2: 1 * i,
+			Param3: 3.14 * float64(i),
+		})
+	}
+}
+
+func remove() {
+	driver.GetDB().Model(&Test{}).Delete(&Test{Id: 1})
+}
+
+func update() {
+	driver.GetDB().Model(&Test{}).
+		Select("param1").
+		Where("id = ?", 2).
+		Updates(&Test{
+			Param1: "updated",
+		})
+}
+
+func query() Test {
+	queryTarget := Test{Id: 2}
+	driver.GetDB().Model(&Test{}).First(&queryTarget)
+	return queryTarget
+}
--- backend/go.mod
+++ backend/go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/gookit/slog v0.5.1
 	github.com/ohanakogo/exceptiongo v0.0.3
 	github.com/ohanakogo/ohanakoutilgo v0.0.3
+	gorm.io/driver/sqlite v1.5.1
+	gorm.io/gorm v1.25.1
 )
 
 require (
@@ -19,12 +21,16 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/gookit/color v1.5.3 // indirect
 	github.com/gookit/gsr v0.0.8 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/ohanakogo/errhandlergo v0.0.2 // indirect
