c2856dbd5d5c1d07cc426567b537e1541a4788cb
kmou424 <me@kmou424.moe>
2023-06-08 20:17:23 +0800

    :hammer_and_wrench: refactor(backend): set default database filepath and optimize path generation

--- backend/bootstrap/start.go
+++ backend/bootstrap/start.go
@@ -3,12 +3,14 @@ package bootstrap
 import (
 	"github.com/TakasakiApps/Narravo/backend/dao"
 	"github.com/TakasakiApps/Narravo/backend/internal/types"
+	"github.com/TakasakiApps/Narravo/backend/internal/utils"
+	"path/filepath"
 )
 
 func Start() {
 	// hardcode database type and path
 	dao.DataBaseBootstrap(&dao.DataBaseBootstrapConfig{
 		DriverType: types.SQLITE,
-		Url:        "data.db",
+		Url:        filepath.Join(utils.EnsureDir(utils.GetAppHomeDir()), "data.db"),
 	})
 }
